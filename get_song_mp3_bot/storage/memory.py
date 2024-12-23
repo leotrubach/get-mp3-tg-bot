45312743b346da1d5da9a66b41750ef6b0f7b316
Arkasha <a.naumenko1@list.ru>
2024-12-24 02:42:33 +0500

    что то сделал

--- get_song_mp3_bot/storage/memory.py
+++ get_song_mp3_bot/storage/memory.py
@@ -1,37 +1,67 @@
+import dataclasses
 import re
+from typing import NamedTuple
 
 import yt_dlp
 from yt_dlp import YoutubeDL
+
+from constants import max_reults
+
+
+class SearchResult(NamedTuple):
+    url: str
+    name: str
+
+
+@dataclasses.dataclass
+class SearchCallbackData:
+    links: dict[str, SearchResult]
+
+
+@dataclasses.dataclass
+class VideoResCallbackData:
+    links: list[str]
+
+
+@dataclasses.dataclass
+class UserState:
+    search_callbacks: dict[str, SearchCallbackData]
+    video_callbacks: dict[str, VideoResCallbackData]
 
 
 class MemoryList:
 
     def __init__(self):
-        self.urls_list = {}
-
-        # user_id : video_name
-        self.url_name_video = {}
+        self.user_state: dict[int, UserState] = {}
 
     def get_urls(self, querty, user_id):
-        max_results = 3
-        ydl_opts = {"format": "best", "noplaylist": True, "quiet": True}
+        max_result = max_reults
+        ydl_opts = {"format": "bestaudio", "noplaylist": True, "quiet": True}
 
         # Use ytsearch to perform the search
-        search_term = f"ytsearch{max_results}:{querty}"
+        search_term = f"ytsearch{max_result }:{querty}"
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             search_results = ydl.extract_info(search_term, download=False)
-        self.urls_list[user_id] = [
+        list_urls = [
             video["webpage_url"] for video in search_results.get("entries", [])
         ]
-        self.url_name_video[user_id] = [
-            video["title"] for video in search_results.get("entries", [])
-        ]
+        list_names = [video["title"] for video in search_results.get("entries", [])]
+        search_results = []
 
-        return self.urls_list[user_id], self.url_name_video[user_id]
+        for i in range(len(list_names)):
+            search_results.append(SearchResult(url=list_urls[i], name=list_names[i]))
 
-    def get_download(self, num, user_id):
-        url = self.urls_list[user_id][num - 1]
-        name_video = self.url_name_video[user_id][num - 1]
+        if user_id not in self.user_state:
+            self.user_state[user_id] = UserState(
+                search_callbacks={}, video_callbacks={}
+            )
+
+        print(list_urls, list_names)
+        return list_urls, list_names, search_results
+
+    def get_download(self, uuid, user_id):
+        name_video = self.user_state[user_id].search_callbacks[uuid].name
+        url = self.user_state[user_id].search_callbacks[uuid].url
         name_video = re.sub(r'[\\/*?:"<>|]', "", name_video)
         options = {
             "format": "bestaudio/best",
@@ -45,30 +75,44 @@ class MemoryList:
                 }
             ],
         }
-
         with YoutubeDL(options) as ydl:
             ydl.download([url])
         return name_video
 
-    def get_download_url(self, url):
-        ydl_opts = {"quiet": True}
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            info_dict = ydl.extract_info(url, download=False)
-            video_title = info_dict.get("title", None)
-        name_video = re.sub(r'[\\/*?:"<>|]', "", video_title)
-        options = {
-            "format": "bestaudio/best",
-            "outtmpl": f"./youtube_audio/{name_video}.%(ext)s",
-            "ffmpeg_location": "C:/ffmpeg/bin",
-            "postprocessors": [
-                {
-                    "key": "FFmpegExtractAudio",
-                    "preferredcodec": "mp3",
-                    "preferredquality": "192",
-                }
-            ],
-        }
-
-        with YoutubeDL(options) as ydl:
-            ydl.download([url])
-        return name_video
+    # def get_download_url(self, url):
+    #     ydl_opts = {"quiet": True}
+    #     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    #         info_dict = ydl.extract_info(url, download=False)
+    #         video_title = info_dict.get("title", None)
+    #     name_video = re.sub(r'[\\/*?:"<>|]', "", video_title)
+    #     options = {
+    #         "format": "bestaudio/best",
+    #         "outtmpl": f"./youtube_audio/{name_video}.%(ext)s",
+    #         "ffmpeg_location": "C:/ffmpeg/bin",
+    #         "postprocessors": [
+    #             {
+    #                 "key": "FFmpegExtractAudio",
+    #                 "preferredcodec": "mp3",
+    #                 "preferredquality": "192",
+    #             }
+    #         ],
+    #     }
+    #
+    #     with YoutubeDL(options) as ydl:
+    #         ydl.download([url])
+    #     return name_video
+    #
+    # def get_videoe(self, url):
+    #     ydl_opts = {"quiet": True}  # Отключить вывод
+    #     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    #         info_dict = ydl.extract_info(url, download=False)
+    #         formats = info_dict.get("formats", [])
+    #         resolutions = set()  # Используем множество, чтобы исключить дубли
+    #
+    #         for fmt in formats:
+    #             if fmt.get("vcodec") != "none":  # Только форматы с видеокодеком
+    #                 resolution = fmt.get("height")
+    #                 if resolution:  # Проверяем, что разрешение существует
+    #                     resolutions.add(f"{resolution}p")
+    #
+    #     return resolutions
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import asyncio
 import logging
 import sys
+import uuid
 from os import getenv
 
 import dotenv
@@ -9,15 +10,14 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import Command
 from aiogram.filters.command import CommandObject
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from aiogram.types import Message, BufferedInputFile
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, BufferedInputFile
+from aiogram.types import Message
 
-from get_song_mp3_bot.storage.constants import max_reults
 from get_song_mp3_bot.storage.memory import MemoryList
 
 dotenv.load_dotenv()
 TOKEN = getenv("BOT_TOKEN")
-bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.MARKDOWN))
 
 storage = MemoryList()
 
@@ -31,25 +31,54 @@ async def cmd_start(message: Message):
     )
 
 
+#
+# @dp.message(Command("video"))
+# async def get_video(message: Message, command: CommandObject):
+#     url = command.args
+#     x = storage.get_videoe(url)
+#     print(x)
+#     buttons = [[InlineKeyboardButton(text=i, callback_data=str(i))] for i in x]
+#     keyboard = InlineKeyboardMarkup(inline_keyboard=buttons)
+#
+#     await message.answer(
+#         f"Выбирете 1 из предложеных качеств:",
+#         parse_mode="Markdown",
+#         reply_markup=keyboard,
+#     )
+#
+
+
 @dp.message(Command("get"))
-async def get_choice(message: types.Message, command: CommandObject):
+async def get_choice(message: Message, command: CommandObject):
     query = command.args
     if not query:
         await message.answer("Вы не написали название")
     await message.answer("Ищу видео . . .")
     user_id = message.from_user.id
-    urls_list, name_video = storage.get_urls(query, user_id)
+    urls_list, name_video, search_results = storage.get_urls(query, user_id)
+    search_callbacks = {}
+    buttons = []
+    for i, result in enumerate(search_results, 1):
+        result_uuid = str(
+            uuid.uuid4()
+        )  # Генерация уникального uuid для каждого результата
+        callback_data = f"search/{result_uuid}/{str(i)}"
+        print(callback_data, len(callback_data))
+        buttons.append(
+            InlineKeyboardButton(text=f"{i} ссылка", callback_data=callback_data)
+        )
+        search_callbacks[result_uuid] = result  # Сохраняем в словарь по uuid
+        # Добавляем данные о поисковых результатах в состояние пользователя
+    storage.user_state[user_id].search_callbacks.update(search_callbacks)
+
     ccc = {}
     for i in range(len(name_video)):
         ccc[name_video[i]] = urls_list[i]
     urls_list = [f"[{name}]({url})" for name, url in ccc.items()]
     urls_str = "\n".join([f"{i}. {item}" for i, item in enumerate(urls_list, 1)])
     print(urls_str)
-    buttons = [
-        [InlineKeyboardButton(text=f"{i} ссылка", callback_data=str(i))]
-        for i in range(1, max_reults + 1)
-    ]
-    keyboard = InlineKeyboardMarkup(inline_keyboard=buttons)
+
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[buttons])
 
     await message.answer(
         f"Выбирете 1 из предложеных ссылок: \n{urls_str}",
@@ -58,34 +87,27 @@ async def get_choice(message: types.Message, command: CommandObject):
     )
 
 
-@dp.message()
-async def download_by_url(message: Message):
-    url = message.text
-    name = storage.get_download_url(url)
-    path = f"C:/Users/Гала/PycharmProjects/get_song_mp3_bot/youtube_audio/{name}.mp3"
-    audio = BufferedInputFile.from_file(path=path, filename=f"{name}.mp3")
-    await bot.send_audio(chat_id=message.chat.id, audio=audio)
+# @dp.message()
+# async def download_by_url(message: Message):
+#     url = message.text
+#     name = storage.get_download_url(url)
+#     path = f"C:/Users/Гала/PycharmProjects/get_song_mp3_bot/youtube_audio/{name}.mp3"
+#     audio = BufferedInputFile.from_file(path=path, filename=f"{name}.mp3")
+#     await bot.send_audio(chat_id=message.chat.id, audio=audio)
 
 
 @dp.callback_query()
 async def callback_handler(callback_query: types.CallbackQuery):
-    button_number = callback_query.data
-    num = int(button_number)
+    data = callback_query.data
+    print(data)
+    scope, reply_uuid, btn_id = data.split("/")
     user_id = callback_query.from_user.id
-    try:
+    if scope == "search":
         await callback_query.message.answer("Загружаю . . .")
-        name_video = storage.get_download(num, user_id)
-
-    except KeyError:
-        await callback_query.message.answer(
-            "ОШИБКА. Сначало введите что хотите скачать `/get название ролика`"
-        )
-        return
-    path = (
-        f"C:/Users/Гала/PycharmProjects/get_song_mp3_bot/youtube_audio/{name_video}.mp3"
-    )
-    audio = BufferedInputFile.from_file(path=path, filename=f"{name_video}.mp3")
-    await bot.send_audio(chat_id=callback_query.message.chat.id, audio=audio)
+        name_video = storage.get_download(reply_uuid, user_id)
+        path = f"C:/Users/Гала/PycharmProjects/get_song_mp3_bot/youtube_audio/{name_video}.mp3"
+        audio = BufferedInputFile.from_file(path=path, filename=f"{name_video}.mp3")
+        await bot.send_audio(chat_id=callback_query.message.chat.id, audio=audio)
 
 
 async def main() -> None:
